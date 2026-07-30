0278fd2fd7b86ef486f02747e90a1a46e2e6b312
Ajay Kumar S <ajaykumarreddy7411@gmail.com>
2026-07-30 18:17:54 +0530

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans=new ArrayList<>();
+        combinations(1,n,k,new ArrayList<>(),ans);
+        return ans;
+    }
+
+    private void combinations(int num,int n,int k,List<Integer> temp,List<List<Integer>> ans){
+        
+        if(temp.size()==k){
+            ans.add(new ArrayList<>(temp));
+            return ;
+        }
+
+        for(int i=num;i<=n;i++){
+            temp.add(i);
+            combinations(i+1,n,k,temp,ans);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
